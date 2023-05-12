4d34b31c429e1387c620cc018535df43bc2665a8
Aditya Pawatekar <130128481+Adityapawatekar@users.noreply.github.com>
2023-05-12 09:35:22 +0530

    Create om.php
    Q2

---
+++ om.php
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<students>
+  <student>
+    <id>001</id>
+    <name>John Doe</name>
+    <age>21</age>
+    <major>Computer Science</major>
+    <gpa>3.5</gpa>
+  </student>
+  <student>
+    <id>002</id>
+    <name>Jane Smith</name>
+    <age>20</age>
+    <major>Engineering</major>
+    <gpa>3.8</gpa>
+  </student>
+  <student>
+    <id>003</id>
+    <name>Bob Johnson</name>
+    <age>22</age>
+    <major>Business Administration</major>
+    <gpa>3.2</gpa>
+  </student>
+  <student>
+    <id>004</id>
+    <name>Sara Lee</name>
+    <age>19</age>
+    <major>Mathematics</major>
+    <gpa>3.9</gpa>
+  </student>
+  <student>
+    <id>005</id>
+    <name>Mike Johnson</name>
+    <age>20</age>
+    <major>Psychology</major>
+    <gpa>3.6</gpa>
+  </student>
+</students>
